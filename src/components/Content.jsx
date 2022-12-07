7332f99c11eacd5b2e327281806900d9d62d35a2
Anna Kondrateva <85047120+AnnaKondrDeveloper@users.noreply.github.com>
2022-12-07 12:28:30 -0500

    first commit

---
+++ src/components/Content.jsx
@@ -0,0 +1,14 @@
+const Content = () => {
+	return (
+		<div className="content">
+			<h2>Content</h2>
+			<img className="content_wall" alt="dandelion" src="https://skinali.photo-clip.ru/images/phocagallery/kategor-skinali/Raznoe/thumbs/phoca_thumb_l_skinali%202805.jpg"></img>
+			<img className="content_avatar" alt="avatar" src="https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg"></img>
+			<p>description</p>
+			<p>My post</p>
+			<p>My message</p>
+		</div>
+	)
+}
+
+export default Content;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+	return (
+		<nav className="app-nav">
+			<div>
+				<a>Profile</a> 
+			</div>
+			<div><a>Messages</a> </div>
+			<div> <a>News</a></div>
+			<div> <a>Music</a></div>
+			<div> <a>Settings</a></div>
+		</nav>
+	)
+}
+
+export default Navbar;
