214a21b19d3680e045adcd88753b33e8d6700bba
Anna Kondrateva <85047120+AnnaKondrDeveloper@users.noreply.github.com>
2023-01-10 14:26:55 -0500

    Use hook useEffect in InfoStatus

--- src/components/Profile/Info/InfoStatusWithHooks.jsx
+++ src/components/Profile/Info/InfoStatusWithHooks.jsx
@@ -1,40 +1,48 @@
-import { useState } from "react";
-import style from "../Info/Info.module.css"
-
+import { useEffect, useState } from "react";
+import style from "../Info/Info.module.css";
 
 const InfoStatusWithHooks = (props) => {
+  const [editMode, setEditMode] = useState(false);
+  const [status, setStatus] = useState(props.status);
 
-	const [editMode, setEditMode] = useState(false);
-	const [status, setStatus] = useState(props.status);
+  useEffect(() => {
+    setStatus(props.status);
+  }, [props.status]);
 
-	const activateEditMode = () => {
-		setEditMode(true);
-	};
+  const activateEditMode = () => {
+    setEditMode(true);
+  };
 
-	const deactivateEditMode = () => {
-		setEditMode(false);
-		props.updateStatus(status)
-	};
+  const deactivateEditMode = () => {
+    setEditMode(false);
+    props.updateStatus(status);
+  };
 
-	const onStatusChange = (even) => {
-		setStatus(even.currentTarget.value);
-	}
+  const onStatusChange = (even) => {
+    setStatus(even.currentTarget.value);
+  };
 
-	return (
-		<div className={style.mystatus}>
-			{!editMode &&
-				<div >
-					<h3 onDoubleClick={activateEditMode}>{props.status || "Just keep swimming"}</h3>
-				</div>
-			}
-			{editMode &&
-				<div>
-					<input onChange={onStatusChange} onBlur={deactivateEditMode} autoFocus={true} value={status} />
-				</div>
-			}
-		</div>
-		)
-
-}
+  return (
+    <div className={style.mystatus}>
+      {!editMode && (
+        <div>
+          <h3 onDoubleClick={activateEditMode}>
+            {props.status || "Just keep swimming"}
+          </h3>
+        </div>
+      )}
+      {editMode && (
+        <div>
+          <input
+            onChange={onStatusChange}
+            onBlur={deactivateEditMode}
+            autoFocus={true}
+            value={status}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default InfoStatusWithHooks;
